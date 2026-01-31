eb1cfcfb245869ce58f78ac30a1c67699fccc295
mina <minammakram@gmail.com>
2026-01-31 23:58:52 +0200

    new

--- 1_Questions_&_Basics/9_Practice_Problem_4.sql
+++ 1_Questions_&_Basics/9_Practice_Problem_4.sql
@@ -1,27 +1,25 @@
 /*
-Query Goal: Find Data Analyst job postings from the first quarter (Jan-Mar) 
+Query Goal: Find Data Analyst job postings from the first quarter (Jan–Mar) 
 with a salary above $70k. 
 - Combine Q1 tables
 - Filter for 'Data Analyst' and salary > 70,000
 - Order by salary descending
 */
+
 SELECT
     quarter1_job_postings.job_location,
-    quasql_courserter1_job_postings.job_via,
+    quarter1_job_postings.job_via,
     quarter1_job_postings.job_posted_date::DATE,
     quarter1_job_postings.salary_year_avg
 FROM (
-    SELECT *
-    FROM jan_jobs
+    SELECT * FROM jan_jobs
     UNION ALL
-    SELECT *
-    FROM feb_jobs
+    SELECT * FROM feb_jobs
     UNION ALL
-    SELECT *
-    FROM mar_jobs
+    SELECT * FROM mar_jobs
 ) AS quarter1_job_postings
 WHERE
-    quarter1_job_postings.salary_year_avg > 70000 AND
-    quarter1_job_postings.job_title_short = 'Data Analyst'
+    quarter1_job_postings.salary_year_avg > 70000
+    AND quarter1_job_postings.job_title_short = 'Data Analyst'
 ORDER BY
-    quarter1_job_postings.salary_year_avg DESC
+    quarter1_job_postings.salary_year_avg DESC;
